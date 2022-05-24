6a212da34ca33c3957a75e30098afe2c7dc02ccf
Iliana Bakalova <thecatraf@gmail.com>
2022-05-24 14:00:05 +0300

    Method to find gender of the person
    Implemented a method to find the gender of person based on the 9th
    character of the egn. If it is odd - the person is female, and when even
    - the person is male.

--- ExtractBirthdayFromEGN/src/Main.java
+++ ExtractBirthdayFromEGN/src/Main.java
@@ -33,14 +33,93 @@ public class Main {
 	    char[] egnArray = egn.toCharArray();
 	    
 	    
-	    System.out.println("You were born on"  + " " + getDate(egn) + getEndingOfDate(egn) + " " +getMonth(egn) + " " +  getYear(egn, egnArray)) ;
+	    System.out.println("You are a "+ getGender(egn) + " and you were born on"  + " " + getDate(egn) + getEndingOfDate(egn) 
+	    + " " + getMonth(egn) + " " + getYear(egn, egnArray) + " in " + getCity(egn) + "." );
 	    
-	    
+	    ;
 	}  
 	
 
 
     } // **
+
+
+
+    private static String getGender(String egn) {
+	String gender ="";
+	int genderCheck = Integer.parseInt(egn.substring(8,9));
+	if(genderCheck%2==0) {
+	gender = "male";
+	} else {
+	gender = "female";
+	}
+	return gender;
+    }
+    
+
+
+private static String getCity(String egn) {
+	long city = Long.parseLong(egn.substring(6,9));
+	String cityString ="";
+	if(city >= 00 && city<=43) {
+	cityString = "Blagoevgrad";
+	}else if (city >= 44 && city<=93) {
+	cityString = "Burgas";
+	}else if (city >= 94 && city<=139) {
+		cityString = "Varna";
+	}else if (city >= 140 && city<=169) {
+		cityString = "Veliko Turnovo";
+	}else if (city >= 170 && city<=183) {
+		cityString = "Vidin";
+	}else if (city >= 184 && city<=217) {
+		cityString = "Vraca";
+	}else if (city >= 218 && city<=233) {
+		cityString = "Gabrovo";
+	}else if (city >= 234 && city<=281) {
+		cityString = "Kurdzhaly";
+	}else if (city >= 282 && city<=301) {
+		cityString = "Kyustendil";
+	}else if (city >= 302 && city<=319) {
+		cityString = "Lovech";
+	}else if (city >= 320 && city<=341) {
+		cityString = "Montana";
+	}else if (city >= 342 && city<=377) {
+		cityString = "Pazardzhik";
+	}else if (city >= 378 && city<=395) {
+		cityString = "Pernik";
+	}else if (city >= 396 && city<=435) {
+		cityString = "Pleven";
+	}else if (city >= 436 && city<=501) {
+		cityString = "Razgrad";
+	}else if (city >= 528 && city<=555) {
+		cityString = "Ruse";
+	}else if (city >= 556 && city<=575) {
+		cityString = "Silistra";
+	}else if (city >= 576 && city<=601) {
+		cityString = "Sliven";
+	}else if (city >= 602 && city<=623) {
+		cityString = "Smolyan";
+	}else if (city >= 624 && city<=721) {
+		cityString = "Sofia - grad";
+	}else if (city >= 722 && city<=751) {
+		cityString = "Sofia - oblast";
+	}else if (city >= 752 && city<=789) {
+		cityString = "Stara Zagora";
+	}else if (city >= 790 && city<=821) {
+		cityString = "Dobrich";
+	}else if (city >= 822 && city<=843) {
+		cityString = "Turgovishte";
+	}else if (city >= 844 && city<=871) {
+		cityString = "Haskovo";
+	}else if (city >= 872 && city<=903) {
+		cityString = "Shumen";
+	}else if (city >= 904 && city<=925) {
+		cityString = "Yambol";
+	}else if (city >= 926 && city<=999) {
+		cityString = "Unknown location";
+	}
+	return cityString;
+}
     
     private static String getEndingOfDate(String egn) {
    	String endingOfDate = "";
@@ -73,9 +152,9 @@ public class Main {
 	String monthCheck = egn.substring(2,3); //2 index
 	String month2 = egn.substring(3,4); //3 index
 	String monthString =""; //month in words
-	//System.out.println(monthPart);
+	
 	//January, February, March, April, May, June, July, August, 
-	//September, October, November, and December
+	//September, October, November, December
 	
 	if(monthCheck.equals("2") || monthCheck.equals("4") || monthCheck.equals("0")) {
 	switch (month2) {
@@ -138,20 +217,17 @@ public class Main {
     private static int getYear(String egn, char[] egnArray) {
 	String yearPart = egn.substring(0,2);
 	String yearCheck = egn.substring(2,3);
-	//System.out.println(yearCheck);
 	int year;
+	
 	if (yearCheck.equals("4") || yearCheck.equals("5")) {
 	year = 10 * Integer.parseInt(egnArray[0] + "") + Integer.parseInt(egnArray[1] + "") + 2000;
 	return year;
-	//System.out.println(year);
 	} else if (yearCheck.equals("2") || yearCheck.equals("3")) {
 	year = 10 * Integer.parseInt(egnArray[0] + "") + Integer.parseInt(egnArray[1] + "") + 1800;
 	return year;
-	//System.out.println(year);
 	} else {
 	year = 10 * Integer.parseInt(egnArray[0] + "") + Integer.parseInt(egnArray[1] + "") + 1900;
 	return year;
-	//System.out.println(year);
 	
 
     }
